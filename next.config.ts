fdb449025414f788f9293c21b487b7d421d68381
Satyam Yadav <sy415801@gmail.com>
2026-05-01 01:43:44 +0530

    Deploy static site to GitHub Pages

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/Satyam-Devfolio-Website',
-  reactStrictMode: true,
-};
-
-export default nextConfig;
